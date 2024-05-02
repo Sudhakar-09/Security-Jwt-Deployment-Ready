9ffe05718b6d4dfb1bba2ed4a345d23ea002b66d
Sudhakar-09 <129831125+Sudhakar-09@users.noreply.github.com>
2024-05-02 14:22:57 +0530

    Update EmailService.java

--- src/main/java/com/thbs/usercreation/service/EmailService.java
+++ src/main/java/com/thbs/usercreation/service/EmailService.java
@@ -1,7 +1,5 @@
 package com.thbs.usercreation.service;     
-package com.thbs.usercreation.service;
- 
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
